ee738d1c07da349746e3e3730d5d062d17070558
Alexander Alemayhu <alexander@alemayhu.com>
2023-01-19 20:58:19 +0100

    feat: add endpoint for getting API version

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { version } from '../package.json';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -15,8 +16,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "$version"', () => {
+      expect(appController.getVersion()).toBe(version);
     });
   });
 });
